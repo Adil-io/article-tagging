300fbcf3e227c7ade2025c84e2fd92f86dabfbfc
Adil Yusuf <adilyusuf786@gmail.com>
2024-05-29 01:59:20 +0530

    Basic implementation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [highlight, setHighlight] = useState("");
+
+  useEffect(() => {
+    if (text != "") {
+      setHighlight(window.getSelection().toString());
+    }
+    console.log(highlight);
+  });
+
+  // TODO: check this
+  const getHighlightedText = (text, highlight) => {
+    // Split on highlight term and include term into parts, ignore case
+    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+    return (
+      <span>
+        {" "}
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === highlight.toLowerCase()
+                ? { fontWeight: "bold" }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}{" "}
+      </span>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className="container">
+        <CardContent className="content">
+          <TextField
+            id="outlined-multiline-static"
+            label="Paste Text Here"
+            multiline
+            rows={5}
+            value={text}
+            fullWidth
+            onChange={(e) => setText(e.target.value)}
+            onMouseUp={() => getHighlightedText(text, highlight)}
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export {default as SelectTechnique} from './SelectTechnique/SelectTechnique';
